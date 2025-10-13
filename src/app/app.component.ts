ad2c058c8afee830c01a89395eb6c0b43be766f8
Welberrr <welberhenrique.costa@gmail.com>
2025-10-13 03:10:51 -0300

    Feat: Finalizando visual do formulario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,17 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { Cadastro } from './cadastro/cadastro';
+import { Consulta } from './consulta/consulta';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, MatToolbarModule, MatIconModule],
+  imports: [RouterOutlet, RouterLink, MatToolbarModule, MatIconModule, Cadastro, Consulta],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent { // Renomeado para AppComponent (convenção)
+export class AppComponent {
   protected readonly title = signal('crud-angular-material');
 }
 
