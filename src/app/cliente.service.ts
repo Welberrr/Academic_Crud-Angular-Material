5d3e9320e7512d2eaf3b2a5ca3e7077696b03771
Welberrr <welberhenrique.costa@gmail.com>
2025-10-13 23:22:59 -0300

    Feat: Persistindo dados de clientes

--- src/app/cliente.service.ts
+++ src/app/cliente.service.ts
@@ -5,10 +5,22 @@ import { cliente } from './cadastro/cliente';
   providedIn: 'root'
 })
 export class ClienteService {
-  
+  static REPO_CLIENTE = 'clientes';
   constructor() { }
 
   salvar(cliente: cliente){
     console.log(cliente);
+  }
+
+  obterStorage(): cliente[]{
+    const Repositorioclientes = localStorage.getItem(ClienteService.REPO_CLIENTE);
+    if(Repositorioclientes){
+      const clientes: cliente[] =  JSON.parse(Repositorioclientes);
+      return clientes;
+    }
+
+    const clientes: cliente[] = [];
+    localStorage.setItem(ClienteService.REPO_CLIENTE, JSON.stringify(clientes));
+    return clientes;
   }
 }
